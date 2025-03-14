2ef6fe30dad760f7221fb3212c5f1ce4691e08ff
ImOcean <nguyenngocnhan2104@gmail.com>
2025-03-14 21:25:18 +0700

    feat: not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className='w-full h-screen flex justify-center items-center gap-4'>
+      <span className='text-5xl text-[#1AD598]'>404</span> <span>|</span>
+      <span>This page could not be found.</span>
+    </div>
+  );
+}
