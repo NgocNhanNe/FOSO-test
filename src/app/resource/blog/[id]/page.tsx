2ae4cb4fb24668bfcb478422a7ad475b97ac515c
ImOcean <nguyenngocnhan2104@gmail.com>
2025-03-15 14:06:48 +0700

    tweak metadata

--- src/app/resource/blog/[id]/page.tsx
+++ src/app/resource/blog/[id]/page.tsx
@@ -1,20 +1,19 @@
 import { blogs } from '@/app/_lib/data';
 import BlogDetail from './BlogDetail';
 
-export async function generateMetadata({ params }: { params: Promise<{ id: number }> }) {
-  const { id } = await params;
+export async function generateMetadata({ params }: { params: { id: string } }) {
+  const id = Number(params.id);
 
   let title = 'Blog';
 
-  const blog = blogs.find(blog => blog.id == id);
+  const blog = blogs.find(blog => blog.id === id);
 
   if (blog) title = blog.title;
 
   return {
-    title
+    title: `Blog | ${title}`
   };
 }
-
 export default function BlogDetailPage({ params }: { params: Promise<{ id: number }> }) {
   return <BlogDetail params={params}></BlogDetail>;
 }
